4e04b609294dfe9c6d16d421c300bd4d2b08934f
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2018-07-16 17:06:52 +0200

    added hyper params for taxis and machine temp

--- src/main.py
+++ src/main.py
@@ -3,7 +3,10 @@
 from loaders import loader_generic, loader_riccione
 from dataset import Dataset
 from sensors import Sensors
+from SomAnomalyDetector import SomAnomalyDetector
+import numpy as np
 
+np.random.seed(1337)
 
 data = loader_generic("../data/nyc_taxi.csv")
 dataset = Dataset(data, False, False, 5, 1)
@@ -13,7 +16,6 @@ print(data[:10])
 print(dataset[0])
 print(dataset[1])
 print(dataset[3])
-
 
 # test smoothing & normalize
 dataset = Dataset(data, True, True, 5, 1)
@@ -34,3 +36,46 @@ print(dataset[0])
 print(dataset[1])
 print(dataset[3])
 
+######################### TAXIS
+# window_size = 150
+# data = loader_generic("../data/nyc_taxi.csv")
+# dataset = Dataset(data, False, False, window_size, 1)
+# detector = SomAnomalyDetector(10, 10, window_size, sigma=1.0, update_weight=0.1, distance_thres=0.30, frequency_thres=100, ignored_items=300, decay_period=5000, decay_factor=0.5)
+# # detector = SomAnomalyDetector(10, 10, window_size, sigma=1.0, update_weight=0.1, distance_thres=0.30, frequency_thres=100, ignored_items=300, decay_period=5000, decay_factor=0.5)
+# #             no    no   no     si    si    si    si
+# # anomalies: [523, 908, 3935, 8465, 8787, 9929, 10080]
+#
+# anomalies = []
+# for i in range(len(dataset)):
+#     data_point = dataset[i]
+#     anomaly, dist = detector.add_data_point(data_point)
+#     if anomaly and (len(anomalies) == 0 or anomalies[-1] < i - window_size):
+#         anomalies.append(i)
+#         print(i)
+#         print(dist)
+# print(len(anomalies))
+# print(anomalies)
+
+
+######################### MACHINE TEMPERATURE
+# window_size = 100
+# data = loader_generic("../data/machine_temperature_system_failure.csv")
+# dataset = Dataset(data, False, False, window_size, 1)
+#
+# detector = SomAnomalyDetector(10, 10, window_size, sigma=1.0, update_weight=0.1, distance_thres=0.10, frequency_thres=100, ignored_items=300, decay_period=5000, decay_factor=0.5)
+
+# con smoothing,
+# window_size = 101
+# dataset = Dataset(data, True, False, window_size, 1, 1)
+# detector = SomAnomalyDetector(10, 10, window_size, sigma=1.0, update_weight=0.1, distance_thres=0.05, frequency_thres=100, ignored_items=300, decay_period=5000, decay_factor=0.5)
+#
+# anomalies = []
+# for i in range(len(dataset)):
+#     data_point = dataset[i]
+#     anomaly, dist = detector.add_data_point(data_point)
+#     if anomaly and (len(anomalies) == 0 or anomalies[-1] < i - window_size):
+#         anomalies.append(i)
+#         print(i)
+#         print(dist)
+# print(len(anomalies))
+# print(anomalies)
